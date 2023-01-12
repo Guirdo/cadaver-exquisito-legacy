9d127b7ab16ca55668649f7f048385028941d1fa
Guirdo <produccionesaldo@gmail.com>
2023-01-12 15:02:55 -0600

    🧩 Add Footer component

---
+++ components/ui/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer () {
+  return (
+    <footer className="footer">
+      <ul className="footer-list">
+        <li className="footer-list-item">
+          <a href="#" className="footer__link">Privacidad</a>
+        </li>
+        <li className="footer-list-item">
+          <a href="#" className="footer__link">Acerca de</a>
+        </li>
+      </ul>
+      <div>Hecho con ❤️ por <a href="#">Guirdo</a></div>
+    </footer>
+  )
+}
+
+export default Footer
