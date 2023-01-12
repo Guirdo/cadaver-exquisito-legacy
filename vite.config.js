cf99d06d1c6eb31c53ed18de32910632e379939c
Guirdo <produccionesaldo@gmail.com>
2023-01-12 14:17:06 -0600

    🔧 Update test configuration

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    dir: resolve(__dirname, 'test'),
-    environment: 'happy-dom'
+    environment: 'happy-dom',
+    globals: true,
+    reporters: 'verbose'
   }
 })
