3065d38ec10e7d698f5b9351b897a168467492eb
Guirdo <produccionesaldo@gmail.com>
2023-01-13 12:12:02 -0600

    🧩 Add SignUpForm component

---
+++ components/LandingPage/SignUpForm.jsx
@@ -0,0 +1,97 @@
+import { useState } from 'react'
+import useForm from '@guirdo/simple-use-form'
+import validator from 'validator'
+
+function SignUpForm ({ setHasLoggedIn }) {
+  const [withPassword, setWithPassword] = useState(false)
+  const { formValues, handleOnChange } = useForm({
+    email: '',
+    password: '',
+    confirmPassword: ''
+  })
+
+  const { email, password, confirmPassword } = formValues
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (isFormValid()) {
+      setHasLoggedIn(true)
+    }
+  }
+
+  const isFormValid = () => {
+    if (!validator.isEmail(email)) return false
+
+    if (withPassword) {
+      if (!validator.isStrongPassword(password)) return false
+      if (password !== confirmPassword) return false
+    }
+
+    return true
+  }
+
+  return (
+    <form
+      aria-label="Sign Up Form"
+      className="login"
+      onSubmit={handleSubmit}
+    >
+      <label
+        className='login__text'
+      >
+        Ingresa con tu correo electrónico y recibiras un mensaje con el link mágico que te permitira crear una cuenta o ingresar si ya lo haz hecho.
+      </label>
+      <input
+        className='login__input'
+        name="email"
+        value={email}
+        onChange={handleOnChange}
+        type="email"
+        placeholder="Correo Electrónico"
+      />
+
+      {
+        withPassword &&
+        <>
+          <input
+            className='login__input'
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleOnChange}
+            aria-label="Password"
+            placeholder="Contraseña"
+          />
+
+          <input
+            className='login__input'
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={handleOnChange}
+            aria-label="Confirm password"
+            placeholder="Confirmar"
+          />
+        </>
+      }
+
+      <span
+        className="login__link"
+        aria-label="with password"
+        onClick={() => setWithPassword(!withPassword)}
+      >
+        Ingresar con contraseña
+      </span>
+
+      <button
+        className='login__button'
+        type="submit"
+      >
+        Entrar
+      </button>
+    </form>
+  )
+}
+
+export default SignUpForm
