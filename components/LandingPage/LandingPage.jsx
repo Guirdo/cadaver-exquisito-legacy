fbde57a3e6e363e458bfed2096c8b87f16ea2b47
Guirdo <produccionesaldo@gmail.com>
2023-01-13 13:38:24 -0600

    🧩 Update LandingPage component * 🚸 Add Signing option: the user can select between signing in or signing up

--- components/LandingPage/LandingPage.jsx
+++ components/LandingPage/LandingPage.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import Layout from '../Layout'
 import SignUpForm from './SignUpForm'
+import SignInForm from './SignInForm'
 
 function LandingPage () {
   const [hasLoggedIn, setHasLoggedIn] = useState(false)
+  const [isSigningIn, setIsSigningIn] = useState(false)
 
   return (
     <Layout>
@@ -13,20 +15,40 @@ function LandingPage () {
         >
           💀Cadaver Exquisito🍷es un juego de palabras donde tus amigos y tú escriben juntos un poema o historia, pero solo podrás ver lo que la persona anterior escribió. Hasta el final de la partida el resultado que verán sera fruto del consciente colectivo de su grupo.
         </p>
-      </main>
 
-      {
-        !hasLoggedIn
-          ? <SignUpForm
-            setHasLoggedIn={setHasLoggedIn}
-          />
-          : <span
-            className='landing__validation-message'
-            aria-label='Validation message'
+        <div>
+          {
+            isSigningIn
+              ? <SignInForm />
+              : <>
+                {
+                  !hasLoggedIn
+                    ? <SignUpForm
+                      setHasLoggedIn={setHasLoggedIn}
+                    />
+                    : <span
+                      className='landing__validation-message'
+                      aria-label='Validation message'
+                    >
+                      Por favor, revisa tu correo
+                    </span>
+                }
+              </>
+          }
+
+          <span
+            className='landing__link'
+            aria-label='signing option'
+            onClick={() => setIsSigningIn(!isSigningIn)}
           >
-            Por favor, revisa tu correo
+            {
+              isSigningIn
+                ? 'Registrate'
+                : 'Inicia sesión'
+            }
           </span>
-      }
+        </div>
+      </main>
     </Layout>
   )
 }
