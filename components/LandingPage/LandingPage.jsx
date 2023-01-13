558e6cf658e453edab385191d92910d0bf6ffbe9
Guirdo <produccionesaldo@gmail.com>
2023-01-12 20:34:41 -0600

    🧩 Improve LandingPage component  * 🚸 Add a brief description of the game * 🚸 Add email validation message

--- components/LandingPage/LandingPage.jsx
+++ components/LandingPage/LandingPage.jsx
@@ -1,9 +1,32 @@
+import { useState } from 'react'
 import Layout from '../Layout'
+import LogInForm from './LogInForm'
 
 function LandingPage () {
+  const [hasLoggedIn, setHasLoggedIn] = useState(false)
+
   return (
     <Layout>
+      <main className='landing'>
+        <p
+          className='landing__text'
+        >
+          💀Cadaver Exquisito🍷es un juego de palabras donde tus amigos y tú escriben juntos un poema o historia, pero solo podrás ver lo que la persona anterior escribió. Hasta el final de la partida el resultado que verán sera fruto del consciente colectivo de su grupo.
+        </p>
+      </main>
 
+      {
+        !hasLoggedIn
+          ? <LogInForm
+          setHasLoggedIn={setHasLoggedIn}
+          />
+          : <span
+            className='landing__validation-message'
+            aria-label='Validation message'
+          >
+            Por favor, revisa tu correo
+          </span>
+      }
     </Layout>
   )
 }
