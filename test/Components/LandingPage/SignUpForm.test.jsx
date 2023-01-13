bb9bea8971a4719317fb4667314eeac4a6569793
Guirdo <produccionesaldo@gmail.com>
2023-01-13 12:11:38 -0600

    ✅ Add SignUpForm test

---
+++ test/Components/LandingPage/SignUpForm.test.jsx
@@ -0,0 +1,52 @@
+import { cleanup, fireEvent, render, screen } from "@testing-library/react";
+import { afterEach, describe, expect, it } from "vitest";
+import SignUpForm from "@/components/LandingPage/SignUpForm";
+
+describe('#SignUpForm', () => {
+  afterEach(() => cleanup())
+
+  it('should be a function', () => {
+    expect(SignUpForm).toBeTypeOf('function')
+  })
+
+  it('should contain a form', () => {
+    render(<SignUpForm />)
+
+    const formElement = screen.getByLabelText('Sign Up Form')
+    expect(formElement).toBeTruthy()
+  })
+
+  it('should contain an input for the user\s email', () => {
+    render(<SignUpForm />)
+
+    const inputElement = screen.getByPlaceholderText('Correo Electrónico')
+    expect(inputElement).toBeTruthy()
+  })
+
+  it('should contain a confirmation button', () => {
+    render(<SignUpForm />)
+
+    const buttonElement = screen.getByRole('button')
+    expect(buttonElement).toBeTruthy()
+    expect(buttonElement.textContent).toBe('Entrar')
+  })
+
+  it('should show a with-password sign up option with a button', () => {
+    render(<SignUpForm />)
+
+    const withPasswordButton = screen.getByLabelText('with password')
+    let passwordInput = screen.queryByPlaceholderText('Contraseña')
+    let confirmPasswordInput = screen.queryByPlaceholderText('Confirmar')
+
+    expect(withPasswordButton).toBeTruthy()
+    expect(passwordInput).not.toBeTruthy()
+    expect(confirmPasswordInput).not.toBeTruthy()
+
+    fireEvent.click(withPasswordButton)
+
+    passwordInput = screen.queryByPlaceholderText('Contraseña')
+    confirmPasswordInput = screen.queryByPlaceholderText('Confirmar')
+    expect(passwordInput).toBeTruthy()
+    expect(confirmPasswordInput).toBeTruthy()
+  })
+})
