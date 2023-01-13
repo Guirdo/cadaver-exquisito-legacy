135a0a351c02a2fdd385df52c8f7d0b9463d9b81
Guirdo <produccionesaldo@gmail.com>
2023-01-12 20:03:24 -0600

    🧩 Add LogInForm component

---
+++ components/LandingPage/LogInForm.jsx
@@ -0,0 +1,43 @@
+import useForm from '@guirdo/simple-use-form'
+function LogInForm () {
+  const { formValues, handleOnChange } = useForm({
+    email: ''
+  })
+
+  const { email } = formValues
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <form
+      aria-label="Log In Form"
+      className="login"
+      onSubmit={handleSubmit}
+    >
+      <label
+        className='login__text'
+      >
+        Ingresa con tu correo electrónico y recibiras un mensaje con el link mágico que te permitira crear una cuenta o ingresar si ya lo haz hecho.
+      </label>
+      <input
+        className='login__input'
+        name="email"
+        value={email}
+        onChange={handleOnChange}
+        type="email"
+        placeholder="Correo Electrónico"
+      />
+
+      <button
+        className='login__button'
+        type="submit"
+      >
+        Entrar
+      </button>
+    </form>
+  )
+}
+
+export default LogInForm
