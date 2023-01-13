6104792f23e3813ccd5aab23ea7760346b27134b
Guirdo <produccionesaldo@gmail.com>
2023-01-13 16:38:16 -0600

    💄 Add Layout styles

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -3,13 +3,13 @@ import Header from './Header'
 
 function Layout ({ children }) {
   return (
-    <>
+    <div className='layout'>
       <Header/>
       {
         children
       }
       <Footer />
-    </>
+    </div>
   )
 }
 
