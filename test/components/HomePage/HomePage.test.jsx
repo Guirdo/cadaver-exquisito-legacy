acf3b8212f5ee717f8c400b1b5f283b65d37efe8
Guirdo <produccionesaldo@gmail.com>
2023-01-14 15:58:22 -0600

    🚚 Fix typo in test/components folder

---
+++ test/components/HomePage/HomePage.test.jsx
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest';
+import HomePage from '@/components/HomePage';
+
+describe('#HomePage', () => {
+  it('should be a function', () => {
+    expect(HomePage).toBeTypeOf('function')
+  })
+})
