1c26a1c7db22bf2402f10d647e2bf3962f223f95
Guirdo <produccionesaldo@gmail.com>
2023-01-14 16:54:37 -0600

    ✅ Add HomePage component test

--- test/components/HomePage/HomePage.test.jsx
+++ test/components/HomePage/HomePage.test.jsx
@@ -1,8 +1,15 @@
 import { describe, it, expect } from 'vitest';
 import HomePage from '@/components/HomePage';
+import { render, screen } from '@testing-library/react';
 
 describe('#HomePage', () => {
   it('should be a function', () => {
     expect(HomePage).toBeTypeOf('function')
+  })
+
+  it('should render a button for creating a new room',()=> {
+    render(<HomePage />)
+    const createNewButton = screen.getByLabelText(/Create new room/i)
+    expect(createNewButton).toBeTruthy()
   })
 })
