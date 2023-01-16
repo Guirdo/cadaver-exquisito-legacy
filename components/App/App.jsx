32d2ce4437658f51eeb5e54ce6a9da2d247b415c
Guirdo <produccionesaldo@gmail.com>
2023-01-16 12:25:43 -0600

    ✅ Update App test

--- components/App/App.jsx
+++ components/App/App.jsx
@@ -1,11 +1,20 @@
 import '@/style/style.scss'
+import { Route, Routes } from 'react-router-dom'
 import LandingPage from '../LandingPage'
+import HomePage from '../HomePage'
 
 function App () {
   return (
-    <>
-      <LandingPage />
-    </>
+    <Routes>
+      <Route
+        path='/'
+        element={<LandingPage />}
+      />
+      <Route
+        path='/home'
+        element={<HomePage />}
+      />
+    </Routes>
   )
 }
 
--- test/components/App/App.test.jsx
+++ test/components/App/App.test.jsx
@@ -1,14 +1,29 @@
 import { afterEach, describe, expect, it } from 'vitest'
 import { cleanup, render, screen } from '@testing-library/react'
 import App from '@/components/App'
+import { MemoryRouter } from 'react-router-dom'
 
 describe('#App', () => {
   afterEach(()=> cleanup())
-  
-  it('should render correctly', () => {
-    render(<App />)
 
-    const headingElement = screen.getByRole('heading', {name: "💀Cadaver Exquisito🍷"})
-    expect(headingElement).toBeTruthy()
+  it('should navigate to the landing page', () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    )
+
+    expect(screen.getByText(/Registrate para jugar/i)).toBeTruthy()
+  })
+
+  it('should navigate to the home page', () => {
+    render(
+      <MemoryRouter initialEntries={['/home']}>
+        <App />
+      </MemoryRouter>
+    )
+
+    const createNewButton = screen.getByLabelText(/Create new room/i)
+    expect(createNewButton).toBeTruthy()
   })
 })
