eba8c4d704aa98ae58c64cddb240f58716b206a0
Guirdo <produccionesaldo@gmail.com>
2023-01-17 16:25:06 -0600

    🗃️ Add profile object to user store

--- store/userStore.js
+++ store/userStore.js
@@ -2,7 +2,9 @@ import { create } from 'zustand'
 
 const useUserStore = create((set) => ({
   user: null,
-  setUser: (user) => set({ user })
+  profile: null,
+  setUser: (user) => set({ user }),
+  setProfile: (profile) => set({ profile })
 }))
 
 export default useUserStore
