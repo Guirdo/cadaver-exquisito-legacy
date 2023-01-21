db29cd902b1a558ca94af4c341393a85e278958e
Guirdo <produccionesaldo@gmail.com>
2023-01-20 20:08:04 -0600

    🌐 Add i18n configuration

--- main.jsx
+++ main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './components/App/App'
+import './lib/i18n'
 
 const rootNode = document.getElementById('root')
 
